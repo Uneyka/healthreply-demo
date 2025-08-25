9954c8918c37b12c247f8a2818bc271d1c956a53
Uneyka <glunz.fabian07@gmail.com>
2025-08-25 15:38:38 +0200

    feat: sidebar + tailwind v4 setup

---
+++ postcss.config.cjs
@@ -0,0 +1,8 @@
+﻿Remove-Item .\postcss.config.cjs -ErrorAction SilentlyContinue
+@'
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {}
+  }
+}
+'@ | Set-Content -Encoding UTF8 .\postcss.config.mjs
